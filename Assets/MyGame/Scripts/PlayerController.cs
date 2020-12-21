5f2b7d9415e831c04d79080a15f32aa3fd3c7b8a
FKrispler <Florian.Krispler03@htl-salzburg.ac.at>
2020-12-21 13:16:06 +0100

    first step Player Controller

---
+++ Assets/MyGame/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public GameObject player;
+
+
+    private float moveHorizontal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveHorizontal = Input.GetAxis("Horizontal") * speed;
+        moveHorizontal = moveHorizontal * Time.deltaTime;
+
+        player.GetComponent<Transform>().position = player.GetComponent<Transform>().position + new Vector3(moveHorizontal, 0, 0);
+    }
+}
